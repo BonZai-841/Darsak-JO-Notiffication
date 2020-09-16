6ef7c6f9dafbab60b221ca2e215160c182d6e19f
BonZai-841 <bakerjalaghef@gmail.com>
2020-09-16 18:33:52 +0300

    using OOP

--- main.py
+++ main.py
@@ -7,82 +7,12 @@ from selenium.webdriver.support import expected_conditions as EC
 import time
 from datetime import date
 import pandas as pd
+from utils import Darsak
+
 
 # ----------- Global Variables --------------
-today_lessons = []
-full_name = []
-grade = []
-website_date = []
-# -------------- Sign in credentials --------------
-national_id = "2001230470"
-year = "2006"
-month = "05"
-day = "13"
-# --------- Use FireFox -----------
-driver = webdriver.Firefox()
-
-# ------------- Open darsak.gov.jo and assert ---------------
-driver.get("https://darsak.gov.jo/auth/login")
-assert "منصة درسك التعليمية" in driver.title
-
-
-def sign_in():
-
-    time.sleep(1)
-    username = driver.find_element_by_id('national_id')
-    username.clear()
-    username.send_keys(national_id)
-
-    time.sleep(0.5)
-    select_year = Select(driver.find_element_by_id("year"))
-    select_year.select_by_visible_text(year)
-
-    time.sleep(0.5)
-    select_month = Select(driver.find_element_by_id("month"))
-    select_month.select_by_visible_text(month)
-
-    time.sleep(0.5)
-    select_day = Select(driver.find_element_by_id("day"))
-    select_day.select_by_visible_text(day)
-
-    time.sleep(1)
-
-    driver.find_element_by_id("loginButton").click()
-
-# ---------- Find Full Name, Grade Today Lessons and Website Date -----------
-
-# ------------- Daily lessons parameters ---------------
-
-
-def get_parameters(today_lessons=today_lessons):
-    global full_name
-    global grade
-    global website_date
-    try:
-
-        full_name = WebDriverWait(driver, 10).until(
-            EC.presence_of_element_located((By.CLASS_NAME, "_auth_user_fullname"))
-
-        ).text
-
-        grade = WebDriverWait(driver, 10).until(
-            EC.presence_of_element_located((By.XPATH, "/html/body/div/div/div[2]/h2"))
-
-        ).text
-
-        website_date = WebDriverWait(driver, 10).until(
-            EC.presence_of_element_located((By.XPATH, "/html/body/div/div/div[2]/div[3]/div/div[2]/p"))
-
-        ).text
-
-        today_lessons_class_name = driver.find_elements_by_class_name("card-title")
-        today_lessons_count = len(today_lessons_class_name)
-
-        for lesson in range(today_lessons_count):
-            today_lessons += today_lessons_class_name[lesson].text.split("\n")
-
-    finally:
-        print("DONE")
+sign_in = Darsak("2001230470", "2006", "05", "13")
+driver = sign_in.driver
 
 
 def convert_to_csv():
@@ -98,6 +28,12 @@ def convert_to_csv():
 
 
 if __name__ == "__main__":
-    sign_in()
-    get_parameters()
+    sign_in.open_website(url="https://darsak.gov.jo/auth/login")
+    sign_in.login_darsak("year", "month", "day", "national_id")
+    sign_in.get_parameters()
+    full_name = sign_in.full_name
+    grade = sign_in.grade
+    today_lessons = sign_in.today_lessons
+    website_date = sign_in.website_date
+
     convert_to_csv()
---
+++ utils.py
@@ -0,0 +1,78 @@
+from selenium import webdriver
+from selenium.webdriver.support.select import Select
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+import time
+from datetime import date
+import pandas as pd
+
+
+class Darsak:
+    def __init__(self, national_num, year, month, day):
+
+        self.year = year
+        self.month = month
+        self.day = day
+        self.national_num = str(national_num)
+        self.driver = webdriver.Firefox()
+        self.today_lessons = []
+        self.full_name = ""
+        self.grade = ""
+        self.website_date = ""
+        self.full_name = ""
+
+    def open_website(self, url):
+        self.driver.get(url)
+        assert "منصة درسك التعليمية" in self.driver.title
+
+    def close_website(self):
+        self.driver.quit()
+        print("DONE")
+
+    def login_darsak(self, year_id, month_id, day_id, national_id):
+        time.sleep(1)
+        self.selector(year_id=year_id, month_id=month_id, day_id=day_id, national_id=national_id)
+        self.driver.find_element_by_id("loginButton").click()
+
+    def selector(self, year_id, month_id, day_id, national_id):
+        username_id = self.driver.find_element_by_id('national_id')
+        username_id.clear()
+        username_id.send_keys(self.national_num)
+
+        time.sleep(0.5)
+        select_year = Select(self.driver.find_element_by_id(year_id))
+        select_year.select_by_visible_text(self.year)
+
+        time.sleep(0.5)
+        select_month = Select(self.driver.find_element_by_id(month_id))
+        select_month.select_by_visible_text(self.month)
+
+        time.sleep(0.5)
+        select_day = Select(self.driver.find_element_by_id(day_id))
+        select_day.select_by_visible_text(self.day)
+
+    def get_parameters(self):
+        # Get the full name , grade, website date, today's lessons
+
+        self.full_name = WebDriverWait(self.driver, 10).until(
+            EC.presence_of_element_located((By.CLASS_NAME, "_auth_user_fullname"))
+
+        ).text
+
+        self.grade = WebDriverWait(self.driver, 10).until(
+            EC.presence_of_element_located((By.XPATH, "/html/body/div/div/div[2]/h2"))
+
+        ).text
+
+        self.website_date = WebDriverWait(self.driver, 10).until(
+            EC.presence_of_element_located((By.XPATH, "/html/body/div/div/div[2]/div[3]/div/div[2]/p"))
+
+        ).text
+
+        today_lessons_class_name = self.driver.find_elements_by_class_name("card-title")
+        today_lessons_count = len(today_lessons_class_name)
+
+        for lesson in range(today_lessons_count):
+            self.today_lessons += today_lessons_class_name[lesson].text.split("\n")
